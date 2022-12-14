df1d5652e5193b149f6dfde472935bddf6a400a9
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-12-14 15:35:22 +0100

    Add automation infrastructure

---
+++ build_and_publish.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+
+cd -P -- "$(dirname -- "$0")"
+./update_counts.py
+cp -t pages/ output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json
+cd pages
+git add output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json
+git commit -m "Automatic update"
+git push
