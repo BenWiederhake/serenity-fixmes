d5d4cde99dd7fd1fc61e08740a226e29ad802685
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-12-14 16:49:06 +0100

    Amend instead of making gh-pages bigger and bigger

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -7,5 +7,5 @@ python3.10 ./update_counts.py
 cp -t pages/ output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json
 cd pages
 git add output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json
-git commit -m "Automatic update"
-git push
+git commit -m "Automatic update" --amend
+git push -f
