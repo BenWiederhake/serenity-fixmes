c905ddd1079be2cf43954f09c73d8f5da0d3299a
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-12-19 19:57:04 +0100

    Define source file list in a single place

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -2,10 +2,25 @@
 
 set -ex
 
+SOURCES="\
+    output_total.png
+    output_day.png
+    output_week.png
+    output_month.png
+    output_year.png
+    tagged_history.json
+    tagged_history.csv
+    cache.json
+    index.html
+    flamegraph.html
+    loc.json
+    todo.json
+"
+
 cd -P -- "$(dirname -- "$0")"
 python3.10 ./update_counts.py
-cp -t pages/ output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json index.html flamegraph.html loc.json todo.json
+cp -t pages/ ${SOURCES}
 cd pages
-git add output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json index.html flamegraph.html loc.json todo.json
+git add ${SOURCES}
 git commit -m "Automatic update" --amend
 git push -f
