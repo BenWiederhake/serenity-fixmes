07093c14ba77ccf57d7ed742d48b04018de41d3c
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-12-19 14:22:39 +0100

    Add a flame graph for all current FIXMEs and SLOC

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -4,8 +4,8 @@ set -ex
 
 cd -P -- "$(dirname -- "$0")"
 python3.10 ./update_counts.py
-cp -t pages/ output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json index.html
+cp -t pages/ output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json index.html flamegraph.html loc.json todo.json
 cd pages
-git add output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json index.html
+git add output_total.png  output_day.png  output_week.png  output_month.png  output_year.png tagged_history.json tagged_history.csv cache.json index.html flamegraph.html loc.json todo.json
 git commit -m "Automatic update" --amend
 git push -f
